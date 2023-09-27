dcbfd59def235ab655542e23f01e68c0906cc218
Andrii Hutnyk <andreigutnik@gmail.com>
2023-09-27 21:35:14 +0200

    Third commit

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import { PhonebookList } from './ContactList.styled';
 import { ContactItem } from 'components/ContactItem/ContactItem';
 import { useSelector } from 'react-redux';
-import { getContacts } from 'redux/selectors';
+import { getContacts, getFilter } from 'redux/selectors';
 
 export function ContactList() {
   const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
+
+  const filtered = contacts.filter(({ name }) =>
+    name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   if (!contacts.length) return <p>The Phonebook is empty!</p>;
   return (
     <PhonebookList>
-      {contacts.map(contact => (
+      {filtered.map(contact => (
         <ContactItem key={contact.id} contact={contact} />
       ))}
     </PhonebookList>
